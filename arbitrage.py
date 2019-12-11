5f931687c3771118debfb82d724477634a33fb26
genox997 <genovese.claudio997@gmail.com>
2019-12-11 11:39:06 +0100

    added driver.quit() at the end of execution

--- arbitrage.py
+++ arbitrage.py
@@ -180,9 +180,30 @@ def catch():
     football()
     basket()
     baseball()
+    driver.quit()
     return odds_opportunities
+
+def print_odds():
+    odds=catch()
+    for odd in odds:
+        if hasattr(odd, 'w_amount'):
+            src_msg = "Match: " + odd.p1  + " vs " + odd.p2 + "\n" \
+                      + "1: " + odd.win + " X: " + odd.draw + " 2: " + odd.lost + "\n" \
+                      + "Bet: " +  str(odd.w_amount) + "(1) " +  str(odd.d_amount) + "(X) " +  str(odd.l_amount) + "(2) \n" \
+                      + "Profit = " + str(odd.profit) + "%"
+
+
+        else:
+            src_msg= "Match: " + odd.p1  + " vs " + odd.p2 + "\n" \
+                      + "1: " + odd.q1 + " 2: " + odd.q2 + "\n" \
+                      + "Bet: " + str(odd.p1_amount) + " (1)  " + str(odd.p2_amount) + " (2)  \n" \
+                      + "Profit = " + str(odd.profit) + "%"
+        print(src_msg)
+        
+
 
 
 if __name__ == "__main__":
-    catch()
+    print_odds()
+    
 
